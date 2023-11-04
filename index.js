92e837bf5a44bddb776c4909586cab4cf7076111
Holly Wheeler <hollyawheeler.96@gmail.com>
2023-11-03 19:14:01 -0700

    lab complete

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(function(response) {
+      console.log(response);
+      return response.json();
+    })
+    .then(function(data) {
+      return renderBooks(data);
+    })
+  }
   
-}
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
